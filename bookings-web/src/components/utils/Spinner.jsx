9782a348ea42a04f638e24fc1e94a2c8a207533b
Stephen Leisy <stephenleisy@Stephens-MacBook-Pro.local>
2021-05-24 12:00:12 -0700

    changed loading spinner for vibe.

--- bookings-web/src/components/utils/Spinner.jsx
+++ bookings-web/src/components/utils/Spinner.jsx
@@ -4,9 +4,10 @@ export default function Spinner() {
   return (
     <div>
       <img
-        src="https://i0.wp.com/codemyui.com/wp-content/uploads/2017/07/fidget-spinner-loading.gif?fit=880%2C440&ssl=1"
+        src="https://i.pinimg.com/originals/91/7e/28/917e28a27d13ffea0a032516149d8dec.gif"
         alt="spinner"
       />
+      <p>Loading...</p>
     </div>
   );
 }
